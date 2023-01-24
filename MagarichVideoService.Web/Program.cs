416981ae2867a56f43e3212f29a3652aca1333d3
Kirill <kirillgroshev52@gmail.com>
2023-01-24 18:41:07 +0300

    Добавление базовых элементов в Web

--- MagarichVideoService.Web/Program.cs
+++ MagarichVideoService.Web/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
