253dd493a19882cd2e098132b4b12dc7aa606d52
Kirill <kirillgroshev52@gmail.com>
2023-02-25 02:07:19 +0300

    Инициализация контроллера пользователей в API проекте

---
+++ MagarichVideoService.Users.API/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MagarichVideoService.Users.API.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Микросервис MagarichVideoService.Users");
+        }
+    }
+}
