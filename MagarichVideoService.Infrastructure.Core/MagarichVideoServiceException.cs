c2ba1eaeb3be06332cb2d80f558fe177c5d6854a
Kirill <kirillgroshev52@gmail.com>
2023-02-26 12:46:37 +0300

    Добавление проекта инфраструктуры содержащего базовые типы системы

---
+++ MagarichVideoService.Infrastructure.Core/MagarichVideoServiceException.cs
@@ -0,0 +1,13 @@
+﻿namespace MagarichVideoService.Infrastructure.Core
+{
+    public class MagarichVideoServiceException : Exception
+    {
+        public MagarichVideoServiceException() { }
+
+        public MagarichVideoServiceException(string message)
+            : base(message) { }
+
+        public MagarichVideoServiceException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
